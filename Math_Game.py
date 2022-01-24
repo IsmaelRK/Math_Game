7df41147e3df6658873ac9b8500fa3d134fe8eab
Ismael <ismael2021ab@gmail.com>
2022-01-24 19:56:39 -0300

    Update Math_Game.py
    Correção de erros

--- Math_Game.py
+++ Math_Game.py
@@ -1,134 +1,141 @@
+from tracemalloc import stop
 import random
-print('Dificuldade 1 tem numeros de 1 a 10 ')
-print('Dificuldade 2 tem numeros de 1 a 50 ')
-print('Dificuldade 3 tem numeros de 1 a 100 ')
-
-print()
-print('Escolha o nivel dificuldade')
-print('1 - 2 - 3')
-
-dificuldade = int(input('digite o nivel da dificuldade: '))
-
-if dificuldade == 1:
-    n = random.randint(1, 10)
-    n2 = random.randint(1, 10)
-
-elif dificuldade == 2:
-    n = random.randint(1, 50)
-    n2 = random.randint(1, 50)
-
-elif dificuldade == 3:
-    n = random.randint(1, 100)
-    n2 = random.randint(1, 100)
-
-#####
-print()
-print('O que quer aprender?')
-print('-----------------------------------------------')
-print('Use 1 para dividir')
-print('Use 2 para adicionar')
-print('Use 3 para multiplicar')
-print('Use 4 para subtrair')
-print()
-questao0 = int(input('Qual das atividades a cima: '))
-
-if questao0 == 1:
-    n3 = n / n2
-    start = (input('Pressione enter '))
 
 
-    if start == '':
-        print(f'{n} / {n2} =')
-        ans = float((input('Sua Resposta: ')))
-        print(f'A resposta era {n3:.2}')
+def math():
 
-    if ans == n3:
-        print("Acertou!")
-    else:
-        print('Errou :(')
+    print('Dificuldade 1 tem numeros de 1 a 10 ')
+    print('Dificuldade 2 tem numeros de 1 a 50 ')
+    print('Dificuldade 3 tem numeros de 1 a 100 ')
 
-    print('Use "s" para concoradar e "n" para descordar')
+    print()
+    print('Escolha o nivel dificuldade')
+    print('1 - 2 - 3')
 
-    questao = input('Tentar novamente? (s ou n) ')
+    dificuldade = int(input('digite o nivel da dificuldade: '))
 
-    if questao == 's':
-        exec(open("project01.py").read())
-    else:
-        exec(exit("project01.py"))
+    if dificuldade == 1:
+        n = random.randint(1, 10)
+        n2 = random.randint(1, 10)
 
-                    # Divisão do código
+    elif dificuldade == 2:
+        n = random.randint(1, 50)
+        n2 = random.randint(1, 50)
 
-elif questao0 == 2:
-    n3 = n + n2
-    start = (input('Pressione enter '))
+    elif dificuldade == 3:
+        n = random.randint(1, 100)
+        n2 = random.randint(1, 100)
 
-    if start == '':
-        print(n, '+', n2, '=')
-        ans = int((input('Sua Resposta: ')))
-        print(f'A resposta era {n3}')
+    #####
+    print()
+    print('O que quer aprender?')
+    print('-----------------------------------------------')
+    print('Use 1 para dividir')
+    print('Use 2 para adicionar')
+    print('Use 3 para multiplicar')
+    print('Use 4 para subtrair')
+    print()
+    questao0 = int(input('Qual das atividades a cima: '))
 
-    if ans == n3:
-        print("Acertou!")
-    else:
-        print('Errou :(')
+                        # Divisão do código (Divisão)
 
-    print('Use "s" para concoradar e "n" para descordar')
+    if questao0 == 1:
+        n3 = n / n2
+        start = (input('Pressione enter '))
 
-    questao = input('Tentar novamente? (s ou n) ')
 
-    if questao == 's':
-        exec(open("project01.py").read())
-    else:
-        exec(exit("project01.py"))
+        if start == '':
+            print(f'{n} / {n2} =')
+            ans = float((input('Sua Resposta: ')))
+            print(f'A resposta era {n3:.2}')
 
-                    # Divisão do código
+        if ans == n3:
+            print("Acertou!")
+        else:
+            print('Errou :(')
 
-elif questao0 == 3:
-    n3 = n * n2
-    start = (input('Pressione enter '))
+        print('Use "s" para concoradar e "n" para descordar')
 
-    if start == '':
-        print(n, '*', n2, '=')
-        ans = int((input('Sua Resposta: ')))
-        print(f'A resposta era {n3}')
+        questao = input('Tentar novamente? (s ou n) ')
 
-    if ans == n3:
-        print("Acertou!")
-    else:
-        print('Errou :(')
+        if questao == 's':
+            math()
+        else:
+            stop
 
-    print('Use "s" para concoradar e "n" para descordar')
+                        # Divisão do código (SOMA)
 
-    questao = input('Tentar novamente? (s ou n) ')
+    elif questao0 == 2:
+        n3 = n + n2
+        start = (input('Pressione enter '))
 
-    if questao == 's':
-        exec(open("project01.py").read())
-    else:
-        exec(exit("project01.py"))
+        if start == '':
+            print(n, '+', n2, '=')
+            ans = int((input('Sua Resposta: ')))
+            print(f'A resposta era {n3}')
 
-        # Divisão do código
+        if ans == n3:
+            print("Acertou!")
+        else:
+            print('Errou :(')
 
-elif questao0 == 4:
-    n3 = n - n2
-    start = (input('Pressione enter '))
+        print('Use "s" para concoradar e "n" para descordar')
 
-    if start == '':
-        print(n, '-', n2, '=')
-        ans = int((input('Sua Resposta: ')))
-        print(f'A resposta era {n3}')
+        questao = input('Tentar novamente? (s ou n) ')
 
-    if ans == n3:
-        print("Acertou!")
-    else:
-        print('Errou :(')
+        if questao == 's':
+            math()
+        else:
+            stop
 
-    print('Use "s" para concoradar e "n" para descordar')
+                        # Divisão do código (Multiplicação)
 
-    questao = input('Tentar novamente? (s ou n) ')
+    elif questao0 == 3:
+        n3 = n * n2
+        start = (input('Pressione enter '))
 
-    if questao == 's':
-        exec(open("project01.py").read())
-    else:
-        exec(exit("project01.py"))
+        if start == '':
+            print(n, '*', n2, '=')
+            ans = int((input('Sua Resposta: ')))
+            print(f'A resposta era {n3}')
 
-##################################################################
+        if ans == n3:
+            print("Acertou!")
+        else:
+            print('Errou :(')
+
+        print('Use "s" para concoradar e "n" para descordar')
+
+        questao = input('Tentar novamente? (s ou n) ')
+
+        if questao == 's':
+            math()
+        else:
+            stop
+
+            # Divisão do código (Subtração)
+
+    elif questao0 == 4:
+        n3 = n - n2
+        start = (input('Pressione enter '))
+
+        if start == '':
+            print(n, '-', n2, '=')
+            ans = int((input('Sua Resposta: ')))
+            print(f'A resposta era {n3}')
+
+        if ans == n3:
+            print("Acertou!")
+        else:
+            print('Errou :(')
+
+        print('Use "s" para concoradar e "n" para descordar')
+
+        questao = input('Tentar novamente? (s ou n) ')
+
+        if questao == 's':
+            math()
+        else:
+            stop
+
+math()
